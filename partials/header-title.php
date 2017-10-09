05cc95600a925558a385a7628ff3dbbf60683b20
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-10-09 17:58:55 -0300

    Renomeação do partil com a parte de título do tema, seguindo a mudança no template pai. Adição de denominação completa e subordinação

---
+++ partials/header-title.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-xs-12">
+        <p class="title-ifrs"><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12">
+        <h1 class="title-campus"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12">
+        <p class="title-mec"><?php _e('Ministério da Educação'); ?></p>
+    </div>
+</div>
