151f23fafb3252c2a7eac86b152148560fbaf2b0
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-10-09 17:14:37 -0300

    Correção no carregamento dos assets.

--- inc/assets.php
+++ inc/assets.php
@@ -3,7 +3,7 @@ function campi_load_styles() {
     /* wp_register_style( $handle, $src, $deps, $ver, $media ); */
     /* wp_enqueue_style( $handle[, $src, $deps, $ver, $media] ); */
 
-    wp_enqueue_style('css-campi', get_template_directory_uri().(WP_DEBUG ? '/css/campi.css' : '/css/campi.min.css'), array(), null, 'all');
+    wp_enqueue_style('css-campi', get_stylesheet_directory_uri().(WP_DEBUG ? '/css/campi.css' : '/css/campi.min.css'), array(), null, 'all');
 }
 
 function campi_load_scripts() {
