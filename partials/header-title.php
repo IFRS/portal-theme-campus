299232d97ec8539a179c57cf373e7811e004524c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-05-15 11:13:25 -0300

    Adição da marca IF no cabeçalho

--- partials/header-title.php
+++ partials/header-title.php
@@ -1,10 +1,17 @@
 <div class="row">
-    <div class="col-xs-12">
-        <p class="title-ifrs"><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></p>
+    <div class="col-md-2 visible-md visible-lg">
+        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/if.png" alt="Logo do IF" class="img-responsive"></a>
     </div>
-</div>
-<div class="row">
-    <div class="col-xs-12">
-        <h1 class="title-campus"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
+    <div class="col-xs-12 col-md-10">
+        <div class="row">
+            <div class="col-xs-12">
+                <p class="title-ifrs"><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <h1 class="title-campus"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
+            </div>
+        </div>
     </div>
 </div>
