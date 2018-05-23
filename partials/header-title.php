72a20cc4361cb58eeaa944ee190c621cd4952b05
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-05-23 17:08:41 -0300

    Ajuste no layout do cabeçalho, mais espaço para o título e para a marca IF

--- partials/header-title.php
+++ partials/header-title.php
@@ -1,8 +1,8 @@
 <div class="row">
-    <div class="col-md-2 visible-md visible-lg">
-        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/if.png" alt="Logo do IF" class="img-responsive"></a>
+    <div class="col-xs-12 col-sm-2 visible-sm visible-md visible-lg">
+        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/if.png" alt="Logo do IF" class="center-block"></a>
     </div>
-    <div class="col-xs-12 col-md-10">
+    <div class="col-xs-12 col-sm-10">
         <div class="row">
             <div class="col-xs-12">
                 <p class="title-ifrs"><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></p>
