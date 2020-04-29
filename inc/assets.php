bfe06400bd0bfd1458a3d68a916a518853a684ef
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-04-29 15:51:13 -0300

    Melhor organização do código com funções anônimas

--- inc/assets.php
+++ inc/assets.php
@@ -1,19 +1,16 @@
 <?php
-function campi_load_styles() {
+add_action( 'wp_enqueue_scripts', function() {
     /* wp_register_style( $handle, $src, $deps, $ver, $media ); */
     /* wp_enqueue_style( $handle[, $src, $deps, $ver, $media] ); */
 
     wp_enqueue_style('css-campi', get_stylesheet_directory_uri().(WP_DEBUG ? '/css/campi.css' : '/css/campi.min.css'), array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/css/campi.min.css'), 'all');
-}
+}, 2 );
 
-function campi_load_scripts() {
+add_action( 'wp_enqueue_scripts', function() {
     /* wp_register_script( $handle, $src, $deps, $ver, $in_footer ); */
     /* wp_enqueue_script( $handle[, $src, $deps, $ver, $in_footer] ); */
 
     if (!is_admin()) {
         wp_deregister_script('jquery');
     }
-}
-
-add_action( 'wp_enqueue_scripts', 'campi_load_styles', 2 );
-add_action( 'wp_enqueue_scripts', 'campi_load_scripts', 2 );
+}, 2 );
