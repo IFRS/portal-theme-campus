5cc9a1337f25015314e90f1c26f2d58b20b32a94
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-04-29 15:50:52 -0300

    Remoção de marcação desnecessária

--- partials/header-title.php
+++ partials/header-title.php
@@ -3,15 +3,7 @@
         <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/if.png" alt="Logo do IF" class="mx-auto"></a>
     </div>
     <div class="col-12 col-md-10">
-        <div class="row">
-            <div class="col-12">
-                <p class="title-ifrs"><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <h1 class="title-campus"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
-            </div>
-        </div>
+        <p class="title-ifrs"><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></p>
+        <h1 class="title-campus"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
     </div>
 </div>
