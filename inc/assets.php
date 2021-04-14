d5ceac95410401a7bace966404e96ecc861ea50c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-04-14 14:55:00 -0300

    Remoção da desabilitação do JQuery do Wordpress

--- inc/assets.php
+++ inc/assets.php
@@ -1,16 +1,12 @@
 <?php
 add_action( 'wp_enqueue_scripts', function() {
+    /* Styles */
     /* wp_register_style( $handle, $src, $deps, $ver, $media ); */
     /* wp_enqueue_style( $handle[, $src, $deps, $ver, $media] ); */
 
     wp_enqueue_style('css-campi', get_stylesheet_directory_uri(). '/css/campi.css', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/css/campi.css'), 'all');
-}, 2 );
 
-add_action( 'wp_enqueue_scripts', function() {
+    /* Scripts */
     /* wp_register_script( $handle, $src, $deps, $ver, $in_footer ); */
     /* wp_enqueue_script( $handle[, $src, $deps, $ver, $in_footer] ); */
-
-    if (!is_admin()) {
-        wp_deregister_script('jquery');
-    }
 }, 2 );
