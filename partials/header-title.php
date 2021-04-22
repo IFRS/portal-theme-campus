7a575aa15b6457a21aad15c8d156f9cb05e0cd57
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-04-22 13:07:56 -0300

    Refatoração do título do cabeçalho

--- partials/header-title.php
+++ partials/header-title.php
@@ -2,13 +2,12 @@
     <h1 class="sr-only"><?php bloginfo('name'); ?></h1>
     <?php the_custom_logo(); ?>
 <?php else : ?>
-    <div class="row">
-        <div class="col-12 col-md-2 d-none d-md-block">
-            <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/if.png" alt="Logo do Instituto Federal" class="mx-auto"></a>
-        </div>
-        <div class="col-12 col-md-10">
-            <p class="title-ifrs"><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></p>
-            <h1 class="title-campus"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
-        </div>
-    </div>
+    <a href="<?php echo esc_url(home_url('/')); ?>" class="title-link">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/if.png" alt="" aria-hidden="true" class="title-img" <?php echo getimagesize(get_stylesheet_directory() . '/img/if.png')[3]; ?>/>
+        <h1 class="title-campus">
+            <small><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></small>
+            <br>
+            <?php bloginfo('name'); ?>
+        </h1>
+    </a>
 <?php endif; ?>
