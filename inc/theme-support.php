f0490973830b7035628d2dfb7c3917191e49b32c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-10-28 17:24:01 -0300

    Remoção de impeditivo para uso de custom logo

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -1,8 +1,5 @@
 <?php
 add_action('after_setup_theme', function()  {
-    // Remove o suporte a cabeçalhos personalizados.
-    remove_theme_support('custom-header');
-
     // Remove menu usado somente no tema principal.
     unregister_nav_menu( 'campi' );
 }, 20);
