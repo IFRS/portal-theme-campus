13ca1f36d552aac74ae6ffd56599e4e1b1a6933b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-12-23 11:41:49 -0300

    Ajuste no nome da imagem para aproveitar o pré-carregamento

--- partials/header-title.php
+++ partials/header-title.php
@@ -3,7 +3,7 @@
     <?php the_custom_logo(); ?>
 <?php else : ?>
     <a href="<?php echo esc_url(home_url('/')); ?>" class="title-link">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/if.png" alt="" aria-hidden="true" class="title-img" <?php echo getimagesize(get_stylesheet_directory() . '/img/if.png')[3]; ?>/>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ifrs.png" alt="" aria-hidden="true" class="title-img" <?php echo getimagesize(get_stylesheet_directory() . '/img/ifrs.png')[3]; ?>/>
         <h1 class="title-campus">
             <small><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></small>
             <br>
